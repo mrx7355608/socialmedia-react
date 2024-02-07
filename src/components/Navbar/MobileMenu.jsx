a97116f217659ccce80eeab9b84e7e7d09f4379d
mrx7355608 <mrx7355608@gmail.com>
2024-02-07 23:15:41 +0500

    created navbar

---
+++ src/components/Navbar/MobileMenu.jsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+
+export default function MobileMenu() {
+    const links = [
+        {
+            path: "/",
+            iconURL: "/home.png",
+        },
+        {
+            path: "/friends",
+            iconURL: "/friends.png",
+        },
+        {
+            path: "/profile",
+            iconURL: "/profile.png",
+        },
+        {
+            path: "/search",
+            iconURL: "/search.png",
+        },
+    ];
+    return (
+        <div className="flex w-full gap-2 p-1 bg-gray-800 border-gray-700 border-t md:hidden">
+            {links.map((link, index) => {
+                return (
+                    <Link to={link.path} key={index} className="flex-1">
+                        <button className="btn bg-transparent border-none w-full outline-none hover:bg-gray-700">
+                            <img
+                                src={link.iconURL}
+                                alt="link"
+                                className="w-5"
+                            />
+                        </button>
+                    </Link>
+                );
+            })}
+        </div>
+    );
+}
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+    return (
+        <div className="navbar bg-gray-800 text-gray-200 px-7">
+            <div className="flex-1">
+                <img src="/logo.png" alt="logo" className="fit-cover w-8" />
+                <Link to="/" className="text-xl ml-1 font-semibold">
+                    Social Media
+                </Link>
+            </div>
+            <div className="flex-none gap-2">
+                <div className="hidden sm:form-control">
+                    <input
+                        type="text"
+                        placeholder="Search"
+                        className="input input-bordered bg-gray-700 text-gray-200 w-auto"
+                    />
+                </div>
+                <div tabIndex={0} className="rounded-full avatar">
+                    <div className="w-10 rounded-full">
+                        <img
+                            alt="user-profile-picture"
+                            src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
