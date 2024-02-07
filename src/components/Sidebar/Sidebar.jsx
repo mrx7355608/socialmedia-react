9f12194473b25a4a88b48d70073ac880ae6bd51b
mrx7355608 <mrx7355608@gmail.com>
2024-02-07 23:46:02 +0500

    created a sidebar

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,58 @@
+import { Link } from "react-router-dom";
+
+export default function Sidebar() {
+    const links = [
+        {
+            path: "/",
+            iconURL: "/home.png",
+            title: "Home",
+        },
+        {
+            path: "/friends",
+            iconURL: "/friends.png",
+            title: "Friends",
+        },
+        {
+            path: "/profile",
+            iconURL: "/profile.png",
+            title: "Profile",
+        },
+        {
+            path: "/settings",
+            iconURL: "/settings.png",
+            title: "Settings",
+        },
+    ];
+    return (
+        <div className="flex flex-col p-4 bg-gray-900 h-screen w-1/4">
+            <UserPictureAndName />
+            {links.map((link, index) => {
+                return (
+                    <Link to={link.path} key={index}>
+                        <div className="bg-transparent flex items-center gap-3 w-full border-none w-full outline-none rounded-lg p-3 hover:bg-gray-800">
+                            <img
+                                src={link.iconURL}
+                                alt="link"
+                                className="w-5"
+                            />
+                            <p className="text-gray-200">{link.title}</p>
+                        </div>
+                    </Link>
+                );
+            })}
+        </div>
+    );
+}
+
+function UserPictureAndName() {
+    return (
+        <div className="p-3 rounded-lg">
+            <img
+                src="/logo.png"
+                alt="user profile picture"
+                className="w-8 fit-cover inline mr-3 rounded-full border border-gray-300"
+            />
+            <span className="text-gray-300 font-medium">Fawad Imran</span>
+        </div>
+    );
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import MobileMenu from "../../components/Navbar/MobileMenu";
 import Navbar from "../../components/Navbar/Navbar";
+import Sidebar from "../../components/Sidebar/Sidebar";
 
 export default function Home() {
     return (
         <>
             <Navbar />
             <MobileMenu />
+            <Sidebar />
         </>
     );
 }
