c2035445b0e35a5a1b35ba5c10b69664fd96905d
mrx7355608 <mrx7355608@gmail.com>
2024-02-08 14:37:45 +0500

    created a login page

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import SocialLoginButtons from "./SocialLoginButtons";
+
+export default function Login() {
+    const [error, setError] = useState("");
+
+    return (
+        <div className="flex flex-col lg:flex-row w-3/4 gap-0 items-center justify-center py-8">
+            <form method="post" className="flex-1 p-0 mt-5">
+                <h1 className="text-3xl font-bold text-gray-200 mb-12">
+                    Login to your account
+                </h1>
+                {error && (
+                    <p className="text-red-900 font-medium p-3 bg-red-200 rounded-lg w-full mb-6">
+                        {error}
+                    </p>
+                )}
+                <input
+                    type="text"
+                    className="input input-bordered w-full bg-transparent border-2 mb-4"
+                    name="email"
+                    placeholder="Email address"
+                />
+                <input
+                    type="text"
+                    placeholder="Password"
+                    className="input input-bordered w-full bg-transparent border-2 mb-1"
+                    name="password"
+                />
+                <p className="text-right text-sm mb-5 hover:underline hover:cursor-pointer">
+                    Forgot Password?
+                </p>
+                <button className="btn btn-primary w-full font-medium rounded-full">
+                    Login
+                </button>
+            </form>
+            <hr className="w-full my-8 lg:w-48 lg:rotate-90 border-gray-800 max-w-md" />
+            <SocialLoginButtons />
+        </div>
+    );
+}
---
+++ src/pages/Login/SocialLoginButtons.jsx
@@ -0,0 +1,30 @@
+export default function SocialLoginButtons() {
+    return (
+        <div className="flex-1 max-w-md">
+            <button className="btn btn-outline w-full font-medium rounded-full relative hover:bg-gray-800 hover:text-gray-200">
+                <img
+                    src="/google.png"
+                    alt="fb-icon"
+                    className="w-6 absolute top-3 left-3"
+                />
+                Continue with Google
+            </button>
+            <button className="btn btn-outline w-full font-medium rounded-full my-4 relative hover:bg-gray-800 hover:text-gray-200">
+                <img
+                    src="/facebook.png"
+                    alt="fb-icon"
+                    className="w-7 absolute top-2.5 left-3"
+                />
+                Continue with Facebook
+            </button>
+            <button className="btn btn-outline w-full font-medium rounded-full relative hover:bg-gray-800 hover:text-gray-200">
+                <img
+                    src="/guest.png"
+                    alt="fb-icon"
+                    className="w-5 absolute top-3 left-4"
+                />
+                Continue as guest
+            </button>
+        </div>
+    );
+}
