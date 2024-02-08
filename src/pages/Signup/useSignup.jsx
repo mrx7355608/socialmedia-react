067c1958e97e5b7233465aa744d935356aaeda09
mrx7355608 <mrx7355608@gmail.com>
2024-02-09 03:27:50 +0500

    used async await in useSignup hook

--- src/pages/Signup/useSignup.jsx
+++ src/pages/Signup/useSignup.jsx
@@ -4,23 +4,27 @@ export default function useSignup() {
     const [loading, setLoading] = useState(false);
     const [apiError, setApiError] = useState("");
 
-    const signup = (signupData) => {
-        setLoading(true);
-        fetch("http://localhost:8000/auth/signup", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(signupData),
-        })
-            .then((resp) => resp.json())
-            .then((data) => {
-                if (!data.ok) {
-                    setApiError(data.error);
-                }
-            })
-            .catch(() => setApiError("An un-expected error occured"))
-            .finally(() => setLoading(false));
+    const signup = async (signupData, changePage) => {
+        try {
+            setLoading(true);
+            const resp = await fetch("http://localhost:8000/auth/signup", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(signupData),
+            });
+            const apiResult = await resp.json();
+            setLoading(false);
+            if (!apiResult.ok) {
+                setApiError(apiResult.error);
+            } else {
+                changePage();
+            }
+        } catch (err) {
+            setApiError(err.message);
+            setLoading(false);
+        }
     };
 
     return { loading, apiError, signup };
