0bb7a02f0d5c31a2e2fcafce47ed57e37a7c56d9
mrx7355608 <mrx7355608@gmail.com>
2024-02-08 20:10:20 +0500

    renamed loading to fullpagespinner

--- src/components/Loading.jsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-    return (
-        <div className="flex items-center justify-center">
-            <span className="loading loading-spinner text-error"></span>;
-        </div>
-    );
-}
