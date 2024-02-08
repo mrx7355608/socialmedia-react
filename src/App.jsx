6b582a81a16882b41b38b8f72decbcf6a9043021
mrx7355608 <mrx7355608@gmail.com>
2024-02-08 13:35:37 +0500

    created auth layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import MainLayout from "./layouts/MainLayout";
+import AuthLayout from "./layouts/AuthLayout";
+import Login from "./pages/Login/Login";
 
 const router = createBrowserRouter([
     {
@@ -10,6 +12,16 @@ const router = createBrowserRouter([
             {
                 index: true,
                 element: <Home />,
+            },
+        ],
+    },
+    {
+        path: "/auth",
+        element: <AuthLayout />,
+        children: [
+            {
+                path: "login",
+                element: <Login />,
             },
         ],
     },
