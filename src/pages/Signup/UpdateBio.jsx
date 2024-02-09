a7bc50c348062c1624d5e6e4d01c64fd8c695e8d
mrx7355608 <mrx7355608@gmail.com>
2024-02-09 06:45:16 +0500

    added update bio functionality

--- src/pages/Signup/UpdateBio.jsx
+++ src/pages/Signup/UpdateBio.jsx
@@ -1,4 +1,30 @@
+import { useState } from "react";
+import Spinner from "../../components/Spinner";
+import { useNavigate } from "react-router-dom";
+
+// eslint-disable-next-line
 export default function UpdateBio() {
+    const [bio, setBio] = useState("");
+    const [loading, setLoading] = useState(false);
+    const navigateTo = useNavigate();
+
+    const updateBio = async () => {
+        setLoading(true);
+        const resp = await fetch("http://localhost:8000/user", {
+            method: "PATCH",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ bio }),
+        });
+        const result = await resp.json();
+        setLoading(false);
+        if (result.ok) {
+            navigateTo("/");
+        }
+    };
+
     return (
         <div className="mx-auto flex items-center justify-center w-1/2 shadow-xl bg-gray-800 h-max p-6 rounded-lg">
             <div className="w-full">
@@ -9,8 +35,11 @@ export default function UpdateBio() {
                     className="textarea textarea-primary resize-none bg-transparent w-full mb-3"
                     placeholder="Let people know about you."
                     rows={4}
+                    onChange={(e) => setBio(e.target.value)}
                 ></textarea>
-                <button className="btn btn-primary">Complete</button>
+                <button className="btn btn-primary" onClick={updateBio}>
+                    {loading ? <Spinner /> : "Complete"}
+                </button>
             </div>
         </div>
     );
