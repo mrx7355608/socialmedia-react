fde74a94e882358a9670789fefff60f45c3f39cc
mrx7355608 <mrx7355608@gmail.com>
2024-02-18 14:31:09 +0500

    added a new boolean prop type

--- src/utils/propTypes.js
+++ src/utils/propTypes.js
@@ -4,3 +4,4 @@ export const stringProp = PropTypes.string.isRequired;
 export const arrayProp = PropTypes.array.isRequired;
 export const funcProp = PropTypes.func.isRequired;
 export const dateProp = PropTypes.instanceOf(Date).isRequired;
+export const booleanProp = PropTypes.bool.isRequired;
