74bec54ecb65e8e605c88ec600c740addf3f388a
mrx7355608 <mrx7355608@gmail.com>
2024-02-18 14:54:36 +0500

    created a hook to fetch timeline

--- src/pages/Home/Timeline/Timeline.jsx
+++ src/pages/Home/Timeline/Timeline.jsx
@@ -3,7 +3,6 @@ import { useEffect } from "react";
 // COMPONENTS
 import Spinner from "../../../components/Spinner";
 import PostCard from "../Post/PostCard";
-import CreatePostBox from "./CreatePostBox";
 
 // HOOKS
 import useAuthFetch from "../../../hooks/useAuthFetch";
@@ -11,18 +10,11 @@ import useAuthFetch from "../../../hooks/useAuthFetch";
 // CONTEXTS
 import PostProvider from "../../../contexts/post";
 import { useTimelineContext } from "../../../contexts/timeline";
+import useTimeline from "./useTimeline";
 
 export default function Timeline() {
     const { timeline, setTimeline } = useTimelineContext();
-    const { loading, error, resp } = useAuthFetch(
-        "http://localhost:8000/posts/timeline"
-    );
-
-    useEffect(() => {
-        if (resp) {
-            setTimeline(resp);
-        }
-    }, [resp, setTimeline]);
+    const { loading, error } = useTimeline(); // Updates timeline context
 
     if (loading) {
         return (
@@ -42,27 +34,18 @@ export default function Timeline() {
 
     return (
         <>
-            <CreatePostBox updateTimeline={updateTimeline} />
-            <div className="flex flex-col items-center justify-start min-h-screen">
-                {timeline.map((post) => {
-                    return (
-                        <PostProvider key={post._id}>
-                            <PostCard
-                                post={post}
-                                removePostFromTimeline={removePostFromTimeline}
-                            />
-                        </PostProvider>
-                    );
-                })}
-            </div>
+            {timeline.map((post) => {
+                return (
+                    <PostProvider key={post._id}>
+                        <PostCard
+                            post={post}
+                            removePostFromTimeline={removePostFromTimeline}
+                        />
+                    </PostProvider>
+                );
+            })}
         </>
     );
-
-    function updateTimeline(newPost) {
-        setTimeline((prev) => {
-            return [newPost, ...prev];
-        });
-    }
 
     function removePostFromTimeline(postID) {
         setTimeline((prev) => prev.filter((p) => p._id !== postID));
---
+++ src/pages/Home/Timeline/useTimeline.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import { useTimelineContext } from "../../../contexts/timeline";
+
+export default function useTimeline() {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
+    const { setTimeline } = useTimelineContext();
+
+    useEffect(() => {
+        setLoading(true);
+        const url = "http://localhost:8000/posts/timeline";
+        const options = {
+            method: "GET",
+            credentials: "include",
+        };
+
+        // Fetch timeline
+        fetch(url, options)
+            .then((resp) => resp.json())
+            .then((result) => setTimeline(result.data))
+            .catch(() => setError("dsfad"))
+            .finally(() => setLoading(false));
+    }, [setLoading, setTimeline]);
+
+    return { loading, error };
+}
