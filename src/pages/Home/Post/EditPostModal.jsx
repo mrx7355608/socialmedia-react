f2939ed87390e16891aa109d040d96c0e1b5488c
mrx7355608 <mrx7355608@gmail.com>
2024-02-19 07:41:10 +0500

    editing post content

--- src/pages/Home/Post/EditPostModal.jsx
+++ src/pages/Home/Post/EditPostModal.jsx
@@ -1,19 +1,86 @@
+import { useState } from "react";
 import { usePostContext } from "../../../contexts/post";
+import Spinner from "../../../components/Spinner";
 
 export default function EditPostModal() {
-    const { post } = usePostContext();
+    const { post, setPost } = usePostContext();
+
+    const [editContent, setEditContent] = useState(post?.content);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
+
     return (
         <dialog id={`edit_post_${post?._id}`} className="modal">
-            <div className="modal-box w-11/12 max-w-5xl">
-                <h3 className="font-bold text-lg">Hello!</h3>
-                <p className="py-4">Click the button below to close</p>
+            <div className="modal-box">
+                <h3 className="font-bold text-xl text-center mb-10">
+                    Edit Post
+                </h3>
+                <div className="flex items-center justify-center w-full max-w-md mx-auto">
+                    <textarea
+                        className="textarea textarea-bordered w-full resize-none"
+                        value={editContent}
+                        onChange={onChangeHandler}
+                        rows={6}
+                    ></textarea>
+                    <p className="text-red-500 mt-1">{error}</p>
+                </div>
                 <div className="modal-action">
-                    <form method="dialog">
-                        {/* if there is a button, it will close the modal */}
-                        <button className="btn">Close</button>
-                    </form>
+                    {loading ? (
+                        <button className="btn btn-primary">
+                            <Spinner />
+                        </button>
+                    ) : (
+                        <button className="btn btn-primary" onClick={editPost}>
+                            Edit
+                        </button>
+                    )}
+                    <button className="btn" onClick={closeEditPostModal}>
+                        Cancel
+                    </button>
                 </div>
             </div>
         </dialog>
     );
+
+    async function editPost() {
+        if (!editContent) {
+            return setError("Nothing to edit");
+        }
+
+        const url = `http://localhost:8000/posts/${post?._id}`;
+        const options = {
+            method: "PATCH",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ content: editContent }),
+        };
+
+        try {
+            setLoading(true);
+            const response = await fetch(url, options);
+            const result = await response.json();
+            setLoading(false);
+            if (result.ok) {
+                setPost((prev) => {
+                    return { ...prev, content: result.data.content };
+                });
+                closeEditPostModal();
+            } else {
+                setError(result.error);
+            }
+        } catch (err) {
+            setError("An un-exepected error occurred");
+        }
+    }
+
+    function onChangeHandler(e) {
+        const { value } = e.target;
+        setEditContent(value);
+    }
+
+    function closeEditPostModal() {
+        document.getElementById(`edit_post_${post?._id}`).close();
+    }
 }
