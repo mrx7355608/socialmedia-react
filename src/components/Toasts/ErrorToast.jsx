eeddfeba0dd84b3132cd0173a499deef69104e89
mrx7355608 <mrx7355608@gmail.com>
2024-02-22 06:42:21 +0500

    adjusted toats positions for mobile design

--- src/components/Toasts/ErrorToast.jsx
+++ src/components/Toasts/ErrorToast.jsx
@@ -2,7 +2,7 @@ import { stringProp } from "../../utils/propTypes";
 
 export default function ErrorToast({ error }) {
     return (
-        <div className="toast toast-top toast-end top-14">
+        <div className="toast toast-top toast-end top-32 lg:top-14">
             <div className="alert alert-error bg-red-400">
                 <span>{error}</span>
             </div>
--- src/components/Toasts/SuccessToast.jsx
+++ src/components/Toasts/SuccessToast.jsx
@@ -2,7 +2,7 @@ import { stringProp } from "../../utils/propTypes";
 
 export default function SuccessToast({ success }) {
     return (
-        <div className="toast toast-top toast-end top-14">
+        <div className="toast toast-top toast-end top-32 lg:top-14">
             <div className="alert alert-success bg-green-400">
                 <span>{success}</span>
             </div>
