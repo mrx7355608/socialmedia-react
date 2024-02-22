f79713d4dc37c1a33475f39a1541ad82ccfcedec
mrx7355608 <mrx7355608@gmail.com>
2024-02-22 07:34:37 +0500

    started working on profile page

---
+++ src/pages/Profile/FriendsSection.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import useAuthFetch from "../../hooks/useAuthFetch";
+import Spinner from "../../components/Spinner";
+
+export default function FriendsSection() {
+    const { loading, error, resp } = useAuthFetch(
+        "http://localhost:8000/friends"
+    );
+
+    return (
+        <div className="p-3 w-full flex flex-wrap items-center justify-start lg:w-1/3 mx-auto">
+            {resp.map((frnd) => {
+                return <FriendProfilePageCard friend={frnd} key={frnd._id} />;
+            })}
+        </div>
+    );
+}
+
+function FriendProfilePageCard({ friend }) {
+    return (
+        <div className="flex items-center justify-between">
+            <div className="flex items-center">
+                <img
+                    src={friend.profilePicture}
+                    alt="profile picture"
+                    className="w-12 h-12 rounded-full object-cover inline mr-3"
+                />
+                <p className="text-md font-bold lg:text-lg text-gray-300 mb-0">
+                    {friend.fullname}
+                </p>
+            </div>
+        </div>
+    );
+}
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,3 +1,39 @@
+import { useUserContext } from "../../contexts/user";
+
 export default function Profile() {
-    return <div>Profile</div>;
+    const { user } = useUserContext();
+
+    return (
+        <div className="flex flex-col px-2.5 py-4 justify-start items-center">
+            {/* Cover picture */}
+            <img
+                src={user.coverPicture}
+                alt="user cover picture"
+                className="w-full object-cover rounded-lg mb-10"
+            />
+            {/* Profile picture and Fullname */}
+            <div className="flex p-3 rounded-lg w-full">
+                <img
+                    src={user.profilePicture}
+                    alt="user profile picture"
+                    className="w-20 h-20 fit-cover inline mr-3 rounded-full border border-gray-300 object-cover"
+                />
+                <div>
+                    <p className="text-gray-300 font-medium text-lg">
+                        {user.fullname}
+                    </p>
+                    {user.bio ? (
+                        <p className="text-gray-400 text-sm">{user.bio}</p>
+                    ) : (
+                        <i className="text-gray-400 text-md mt-0">
+                            No bio provided
+                        </i>
+                    )}
+                </div>
+            </div>
+            {/* Bio */}
+            {/* Friends */}
+            {/* Posts */}
+        </div>
+    );
 }
