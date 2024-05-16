80429cd70d5fdd6b7e01728433cd3e4da876a4e9
mrx7355608 <mrx7355608@gmail.com>
2024-05-16 17:08:22 +0500

    added icons in toasts

--- src/components/toasts/ErrorToast.jsx
+++ src/components/toasts/ErrorToast.jsx
@@ -1,9 +1,11 @@
 import { stringProp } from "../../utils/propTypes";
+import { MdErrorOutline } from "react-icons/md";
 
 export default function ErrorToast({ error }) {
     return (
         <div className="toast toast-top toast-end top-32 lg:top-14">
             <div className="alert alert-error bg-red-400">
+                <MdErrorOutline size={23} />
                 <span>{error}</span>
             </div>
         </div>
--- src/components/toasts/SuccessToast.jsx
+++ src/components/toasts/SuccessToast.jsx
@@ -1,9 +1,11 @@
 import { stringProp } from "../../utils/propTypes";
+import { FaCheck } from "react-icons/fa";
 
 export default function SuccessToast({ success }) {
     return (
         <div className="toast toast-top toast-end top-32 lg:top-14">
             <div className="alert alert-success bg-green-400">
+                <FaCheck size={23} />
                 <span>{success}</span>
             </div>
         </div>
